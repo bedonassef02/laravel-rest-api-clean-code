d7ec4da1322144b366dfc0448cc2be8fbdb45c2b
Bedo <bedonassef71@gmail.com>
2024-10-24 16:09:58 +0300

    rest-api

---
+++ app/Interfaces/ProductRepositoryInterface.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Interfaces;
+
+interface ProductRepositoryInterface
+{
+    public function index();
+
+    public function getById($id);
+
+    public function store(array $data);
+
+    public function update($id, array $data);
+
+    public function delete($id);
+}
